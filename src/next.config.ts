4d7fdee30ab71bf435fceb0fb33bdec7bb4e380f
Wondmagegn Merkeb <wondmagegndev@gmail.com>
2025-07-26 07:27:29 +0000

    [10:24:47.014] Running build in Washington, D.C., USA (East) – iad1 [10:

---
+++ src/next.config.ts
@@ -0,0 +1,30 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // This is to address an issue with pdf.js-extract trying to require 'canvas'
+    // which is a server-side dependency. We can safely ignore it for all builds.
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.fs = false;
+    
+    return config;
+  },
+};
+
+export default nextConfig;
