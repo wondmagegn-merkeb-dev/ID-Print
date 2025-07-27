5f6e914ce694ab6408b57ec2b34c53e4d59fcbee
fikirayele <fikirayele332@gmail.com>
2025-07-27 12:38:12 +0200

    update prisma and package dependencies; add initial migration for User table with new fields

---
+++ prisma/migrations/20250727103704_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'User',
+    "status" TEXT NOT NULL DEFAULT 'Active',
+    "isChangePassword" BOOLEAN NOT NULL DEFAULT false,
+    "referralGiftReceived" BOOLEAN NOT NULL DEFAULT false,
+    "invitedBySource" TEXT NOT NULL DEFAULT 'SELF',
+    "invitedById" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_invitedById_fkey" FOREIGN KEY ("invitedById") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
